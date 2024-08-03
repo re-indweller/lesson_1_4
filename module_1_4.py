3b5bb863a00a1be72f0cba0e2bbe920582c253c4
re-indweller <asf.ayrat@gmail.com>
2024-08-03 12:42:25 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+my_string = input() #
+print(my_string.upper()) # Выведите строку my_string в верхнем регистре.
+print(my_string.lower()) # Выведите строку my_string в нижнем регистре.
+print(my_string.replace(" ", "")) # Измените строку my_string, удалив все пробелы.
+print(my_string[0]) # Выведите первый символ строки my_string.
+print(my_string[-1]) # Выведите последний символ строки my_string.
