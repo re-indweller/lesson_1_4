02901a3e1db72fcc110595c009f38fcc78cc9122
re-indweller <asf.ayrat@gmail.com>
2024-08-04 21:52:16 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,7 @@
-my_string = input() #
-print(my_string.upper()) # Выведите строку my_string в верхнем регистре.
-print(my_string.lower()) # Выведите строку my_string в нижнем регистре.
-print(my_string.replace(" ", "")) # Измените строку my_string, удалив все пробелы.
-print(my_string[0]) # Выведите первый символ строки my_string.
-print(my_string[-1]) # Выведите последний символ строки my_string.
+my_string = input('Напишите любое слово ')  #
+print(len(my_string))  # Вывести количество символов введённого текста
+print(my_string.upper())  # Выведите строку my_string в верхнем регистре.
+print(my_string.lower())  # Выведите строку my_string в нижнем регистре.
+print(my_string.replace(" ", ""))  # Измените строку my_string, удалив все пробелы.
+print(my_string[0])  # Выведите первый символ строки my_string.
+print(my_string[-1])  # Выведите последний символ строки my_string.
